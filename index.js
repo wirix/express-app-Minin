591b08401b8e83891d0dfbb904c6d74dfa5a1fd8
wirix <88577048+wirix@users.noreply.github.com>
2023-06-29 11:00:47 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const exphs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const coursesRoutes = require('./routes/courses')
+const addRoutes = require('./routes/add')
+const app = express()
+
+const hbs = exphs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+// настройка express-handlebars
+// регистрируем что есть такой движок и вторым парам передаем туда значение
+app.engine('hbs', hbs.engine)
+// set- начинаем использовать движок 'hbs', 'view engine' -какой engine хотим использовать
+app.set('view engine', 'hbs')
+// пишем название папки где будут храниться шаблоны (2ой парам по умол views)
+app.set('views', 'views')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/add', addRoutes)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`сервер запущен на порту ${PORT}`)
+})
